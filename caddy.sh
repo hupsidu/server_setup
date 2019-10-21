8046903136f7ad888605335ea8bfb93edea32c54
hupsidu <48177839+hupsidu@users.noreply.github.com>
2019-10-21 20:49:49 +0200

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 
+go version
+
+go get github.com/mholt/caddy/caddy
+cd $GOPATH/src/github.com/mholt/caddy
+TAG=git describe --abbrev=0
+git checkout -b "adding_plugins" $TAG
+
+go install github.com/mholt/caddy/caddy
+
+caddy
