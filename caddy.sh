10f20611d65ef0e5e4e90f5f538bb6cb471a24f3
hupsidu <48177839+hupsidu@users.noreply.github.com>
2019-10-21 21:25:13 +0200

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -10,3 +10,20 @@ git checkout -b "adding_plugins" $TAG
 go install github.com/mholt/caddy/caddy
 
 caddy
+
+sudo cp $GOPATH/bin/caddy /usr/local/bin/
+sudo chown root:root /usr/local/bin/caddy
+sudo chmod 755 /usr/local/bin/caddy
+sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/caddy
+sudo mkdir /etc/caddy
+sudo chown -R root:www-data /etc/caddy
+sudo mkdir /etc/ssl/caddy
+sudo chown -R root:www-data /etc/ssl/caddy
+sudo chmod 0770 /etc/ssl/caddy
+sudo mkdir /var/www
+sudo chown www-data:www-data /var/www
+sudo touch /etc/caddy/Caddyfile
+sudo cp $GOPATH/src/github.com/mholt/caddy/dist/init/linux-systemd/caddy.service /etc/systemd/system/
+sudo chmod 644 /etc/systemd/system/caddy.service
+sudo systemctl daemon-reload
+sudo systemctl status caddy
