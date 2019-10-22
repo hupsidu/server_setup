79eca7cbfe73c7a542ccd265aee2713e19a8ca7e
hupsidu <48177839+hupsidu@users.noreply.github.com>
2019-10-22 18:11:54 +0200

    Update SshMessage.sh

--- SshMessage.sh
+++ SshMessage.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo '#!/bin/bash' > /opt/shell-login.sh
-echo 'echo "Login auf $(hostname) am $(date +%d.%m.%Y) um $(date +%H:%M) Uhr"' > /opt/shell-login.sh
-echo 'echo "Benutzer: $USER"' > /opt/shell-login.sh
-echo 'echo' > /opt/shell-login.sh
+echo 'echo "Login auf $(hostname) am $(date +%d.%m.%Y) um $(date +%H:%M) Uhr"' >> /opt/shell-login.sh
+echo 'echo "Benutzer: $USER"' >> /opt/shell-login.sh
+echo 'echo' >> /opt/shell-login.sh
  
 chmod 755 /opt/shell-login.sh
 echo '/opt/shell-login.sh | mailx -s "SSH Login auf deinem Server" joshy1@live.de' >> /etc/profile
