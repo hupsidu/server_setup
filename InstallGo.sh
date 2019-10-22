278d26f2bd55a6ec35e4d77acc43ca70fd788b85
hupsidu <48177839+hupsidu@users.noreply.github.com>
2019-10-22 16:05:04 +0200

    Update InstallGo.sh

--- InstallGo.sh
+++ InstallGo.sh
@@ -4,10 +4,9 @@ cd ~
 curl -O https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz
 
 tar xvf go1.6.linux-amd64.tar.gz
-sudo chown -R root:root ./go
-sudo mv go /usr/local
+chown -R root:root ./go
+mv go /usr/local
 
-sudo nano ~/.profile
 export GOPATH=$HOME/work
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
 
